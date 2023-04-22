eed2eb50e386edd289fb677fc305b333ea07dba1
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2023-04-22 16:52:10 +0200

    corrected line 6

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      That's us
+      <p>That's us</p>
     </main>
   );
 }
