cb713f830d5d5b75ed5ff353e550a8f6dabc6c97
Maxime Vidal <vmaxmc2@gmail.com>
2023-04-23 12:52:16 +0200

    changed default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Space_Grotesk } from 'next/font/google';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+      :root {
+        --space-grotesk-font: ${spaceGrotesk.style.fontFamily};
+      }
+    `}
+      </style>
+      <Component {...pageProps} />
+    </>
+
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        sans: ['var(--space-grotesk-font)'],
       },
     },
   },
