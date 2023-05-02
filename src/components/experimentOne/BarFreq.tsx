c2e22f7873a44a372cfdc1fa5537642586740816
Maxime Vidal <vmaxmc2@gmail.com>
2023-05-02 13:40:45 +0200

    tooltip only on phone

--- src/components/experimentOne/BarFreq.tsx
+++ src/components/experimentOne/BarFreq.tsx
@@ -6,13 +6,36 @@ import {
   YAxis,
   CartesianGrid,
   ResponsiveContainer,
+  Tooltip,
 } from "recharts";
+import { TooltipProps } from "recharts";
+import {
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
 
 export default function BarFreq({
   frequencies,
   title,
   yDataKey,
 }: BarFreqProps) {
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="md:hidden">
+          <p className="label">{`${label}`}</p>{" "}
+          {/* ${payload[0].value} for y value */}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <div className="mt-16 flex flex-col items-center w-full">
       <h1 className="text font-bold mb-4">{title}</h1>
@@ -29,6 +52,10 @@ export default function BarFreq({
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
+          <Tooltip
+            content={<CustomTooltip />}
+            cursor={{ fill: "transparent" }}
+          />
           <Bar
             dataKey={yDataKey}
             fill="#FFFFFF"
