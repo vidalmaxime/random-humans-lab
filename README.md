d5f6ec2fe1df599f66524fe607791ad685d6f458
Maxime Vidal <vmaxmc2@gmail.com>
2023-05-17 18:26:40 +0200

    rebased

--- README.md
+++ README.md
@@ -4,11 +4,4 @@ Let's find the human random seed
 
 ![MIT License](https://img.shields.io/badge/license-MIT-green)
 
-![Buzz](./assets/rhl_banner.png)
-
-## Run locally
-
-```bash
-npm install
-npm run dev
-```
+![RHL](./assets/rhl_banner.png)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,8 +34,8 @@ export default function Home() {
           workings that underlie our world.
         </p>
         <div className="mt-4 mb-4 md:mt-16 flex flex-col items-start">
-            <ExperimentTile name="experiment 1" path="/experiment-number" />
-            <ExperimentTile name="experiment 2" path="/experiment-click-anywhere" />
+          <ExperimentTile name="experiment 1" path="/experiment-number" />
+          <ExperimentTile name="experiment 2" path="/experiment-click" />
         </div>
         <Footer />
       </main>
