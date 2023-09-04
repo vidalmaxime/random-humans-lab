f026b6cee45da7ac3666a93fe35eb9aa82d3aff6
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2023-09-04 14:18:37 +0200

    corrected multiple click bug with loader

--- src/components/experimentTwo/VizQuestion.tsx
+++ src/components/experimentTwo/VizQuestion.tsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import SkewLoader from "react-spinners/SkewLoader";
 
 export default function VizQuestion({ send }: VizQuestionProps) {
+  const [justClicked, setJustClicked] = useState(false);
+
   return (
     <div className="flex justify-center flex-col items-center w-full h-full">
-      <div
-        className="w-screen h-full cursor-pointer "
-        onClick={(e) => {
-          // Get the offset of the click relative to the svg rectangle on a scale of 0 to 1
-          const x = e.nativeEvent.offsetX / e.currentTarget.clientWidth;
-          const y = e.nativeEvent.offsetY / e.currentTarget.clientHeight;
-          const userAgent = window.navigator.userAgent;
-          console.log(userAgent);
-          let deviceType = "computer";
-          if (/Mobi|Android/i.test(userAgent)) {
-            deviceType = "mobile";
-          }
-          console.log(deviceType);
+      {!justClicked ? (
+        <div
+          className="w-screen h-full cursor-pointer "
+          onClick={(e) => {
+            // Get the offset of the click relative to the svg rectangle on a scale of 0 to 1
+            setJustClicked(true);
+            const x = e.nativeEvent.offsetX / e.currentTarget.clientWidth;
+            const y = e.nativeEvent.offsetY / e.currentTarget.clientHeight;
+            const userAgent = window.navigator.userAgent;
+            console.log(userAgent);
+            let deviceType = "computer";
+            if (/Mobi|Android/i.test(userAgent)) {
+              deviceType = "mobile";
+            }
 
-          send(
-            x,
-            y,
-            e.currentTarget.clientWidth,
-            e.currentTarget.clientHeight,
-            deviceType
-          );
-        }}
-      ></div>
+            send(
+              x,
+              y,
+              e.currentTarget.clientWidth,
+              e.currentTarget.clientHeight,
+              deviceType
+            );
+          }}
+        ></div>
+      ) : (
+        <div>
+          <SkewLoader color="black" speedMultiplier={0.5} />
+        </div>
+      )}
     </div>
   );
 }
